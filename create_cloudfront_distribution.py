d7aa4c45512e3ee7d0a66a973d672a72cb0c4460
Chandrasekar <70361171+chandrasekar-r@users.noreply.github.com>
2023-03-19 08:52:40 +0530

    Update create_cloudfront_distribution.py
    Optimized code

--- create_cloudfront_distribution.py
+++ create_cloudfront_distribution.py
@@ -1,110 +1,110 @@
+import sys
 import boto3
 
-# Create an AWS WAF client
-waf_client = boto3.client('waf')
 
-# Create an IP set
-ip_set_response = waf_client.create_ip_set(
-    Name='my-ip-set',
-    ChangeToken=waf_client.get_change_token()['ChangeToken'],
-    Addresses=[
-        '192.0.2.0/24',
-        '198.51.100.0/24'
-    ]
-)
-ip_set_id = ip_set_response['IPSet']['IPSetId']
+config = {
+    'profile': 'default',
+    'region': 'us-east-1',
+    'account_id': '123456789012',
+    'ip_set_name': 'my-ip-set',
+    'web_acl_name': 'my-web-acl',
+    'distribution_name': 'my-distribution',
+    'origin_domain_name': 'my-bucket.s3.amazonaws.com',
+}
 
-# Create a Web ACL
-web_acl_response = waf_client.create_web_acl(
-    Name='my-web-acl',
-    ChangeToken=waf_client.get_change_token()['ChangeToken'],
-    DefaultAction={
-        'Type': 'BLOCK'
-    },
-    MetricName='my-metric',
-    Rules=[
-        {
-            'Name': 'my-rule',
-            'Priority': 1,
-            'Action': {
-                'Type': 'ALLOW'
-            },
-            'Statement': {
-                'IPSetReferenceStatement': {
-                    'Arn': f'arn:aws:waf::123456789012:ipset/{ip_set_id}',
-                    'IPSetForwardedIPConfig': {
-                        'HeaderName': 'X-Forwarded-For',
-                        'FallbackBehavior': 'MATCH'
-                    }
+
+def create_ip_set(wafv2_client, ip_set_name):
+    response = wafv2_client.create_ip_set(
+        Name=ip_set_name,
+        Scope='REGIONAL',
+        Addresses=['192.0.2.0/24', '198.51.100.0/24'],
+        Tags=[{'Key': 'my-tag', 'Value': 'my-value'}]
+    )
+    return response['IPSet']['ARN']
+
+
+def create_web_acl(wafv2_client, web_acl_name, ip_set_arn):
+    response = wafv2_client.create_web_acl(
+        Name=web_acl_name,
+        Scope='REGIONAL',
+        DefaultAction={'Type': 'BLOCK'},
+        VisibilityConfig={'SampledRequestsEnabled': True, 'CloudWatchMetricsEnabled': True},
+        Rules=[
+            {
+                'Name': 'my-rule',
+                'Priority': 1,
+                'Action': {'Type': 'ALLOW'},
+                'Statement': {
+                    'OverrideAction': {'Type': 'NONE'},
+                    'IPSetReferenceStatement': {'ARN': ip_set_arn}
                 }
             }
-        }
-    ]
-)
-web_acl_id = web_acl_response['WebACL']['WebACLId']
+        ],
+        Tags=[{'Key': 'my-tag', 'Value': 'my-value'}]
+    )
+    return response['WebACL']['ARN']
 
-# Create a CloudFront distribution
-cloudfront_client = boto3.client('cloudfront')
-distribution_response = cloudfront_client.create_distribution(
-    DistributionConfig={
-        'CallerReference': 'my-caller-reference',
-        'Aliases': {
-            'Quantity': 0
-        },
-        'DefaultRootObject': 'index.html',
-        'Origins': {
-            'Quantity': 1,
-            'Items': [
-                {
-                    'Id': 'my-origin',
-                    'DomainName': 'my-bucket.s3.amazonaws.com',
-                    'S3OriginConfig': {
-                        'OriginAccessIdentity': ''
+
+def create_cloudfront_distribution(cloudfront_client, web_acl_arn, origin_domain_name, distribution_name):
+    response = cloudfront_client.create_distribution(
+        DistributionConfig={
+            'CallerReference': 'my-caller-reference',
+            'Aliases': {'Quantity': 0},
+            'DefaultRootObject': 'index.html',
+            'Origins': {
+                'Quantity': 1,
+                'Items': [
+                    {
+                        'Id': 'my-origin',
+                        'DomainName': origin_domain_name,
+                        'S3OriginConfig': {'OriginAccessIdentity': ''}
                     }
-                }
-            ]
-        },
-        'DefaultCacheBehavior': {
-            'TargetOriginId': 'my-origin',
-            'ForwardedValues': {
-                'QueryString': False,
-                'Cookies': {
-                    'Forward': 'none'
-                },
-                'Headers': {
-                    'Quantity': 0
-                },
-                'QueryStringCacheKeys': {
-                    'Quantity': 0
-                }
+                ]
             },
-            'TrustedSigners': {
-                'Enabled': False,
-                'Quantity': 0
+            'DefaultCacheBehavior': {
+                'TargetOriginId': 'my-origin',
+                'ForwardedValues': {
+                    'QueryString': False,
+                    'Cookies': {'Forward': 'none'},
+                    'Headers': {'Quantity': 0},
+                    'QueryStringCacheKeys': {'Quantity': 0}
+                },
+                'TrustedSigners': {'Enabled': False, 'Quantity': 0},
+                'ViewerProtocolPolicy': 'redirect-to-https',
+                'MinTTL': 0
             },
-            'ViewerProtocolPolicy': 'redirect-to-https',
-            'MinTTL': 0
-        },
-        'CacheBehaviors': {
-            'Quantity': 0
-        },
-        'CustomErrorResponses': {
-            'Quantity': 0
-        },
-        'Comment': '',
-        'Logging': {
-            'Enabled': False,
-            'IncludeCookies': False,
-            'Bucket': '',
-            'Prefix': ''
-        },
-        'PriceClass': 'PriceClass_All',
-        'Enabled': True,
-        'ViewerCertificate': {
-            'CloudFrontDefaultCertificate': True
-        },
-        'WebACLId': web_acl_id,
-        'HttpVersion': 'http2'
-    }
-)
-distribution_id = distribution_response['Distribution']['Id']
+            'Enabled': True,
+            'ViewerCertificate': {'CloudFrontDefaultCertificate': True},
+            'WebACLId': web_acl_arn
+        }
+    )
+    return response['Distribution']
+
+if __name__ == '__main__':
+    session = boto3.Session(profile_name=config['profile'])
+    wafv2_client = session.client('wafv2', region_name=config['region'])
+    cloudfront_client = session.client('cloudfront')
+
+    try:
+        ip_set_arn = create_ip_set(wafv2_client, config['ip_set_name'])
+        print(f"IP set created with ARN: {ip_set_arn}")
+    except Exception as e:
+        print(f"Error creating IP set: {e}")
+        sys.exit(1)
+
+    try:
+        web_acl_arn = create_web_acl(wafv2_client, config['web_acl_name'], ip_set_arn)
+        print(f"Web ACL created with ARN: {web_acl_arn}")
+    except Exception as e:
+        print(f"Error creating Web ACL: {e}")
+        sys.exit(1)
+
+    try:
+        distribution = create_cloudfront_distribution(
+            cloudfront_client, web_acl_arn, config['origin_domain_name'], config['distribution_name'])
+        print(f"CloudFront distribution created with ID: {distribution['Id']}")
+    except Exception as e:
+        print(f"Error creating CloudFront distribution: {e}")
+        sys.exit(1)
+
+# Again, make sure to replace the values in the config dictionary with the appropriate values for your own use case before running the code.
